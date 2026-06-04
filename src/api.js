f0e3bf54bdd7957c5929eec0aff762ad7d6c9bc5
expap9 <expap9@gmail.com>
2026-06-04 22:25:40 +0700

    Update API URL for Google Apps Script

--- src/api.js
+++ src/api.js
@@ -1,15 +1,15 @@
 // นำ Web App URL ที่ได้จาก Google Apps Script มาวางในเครื่องหมายคำพูดด้านล่างนี้ครับ
-export const GAS_URL = "นำ_URL_มาวางตรงนี้";
+export const GAS_URL = "https://script.google.com/macros/s/AKfycbwseSsbiGGuweKdpYMQQv78nGxSwp8FVmUr-v193DJc-xxqd3VzMBnvk0krEHdqmJzt/exec";
 
 export const api = {
   get: async (action) => {
-    if (GAS_URL === "นำ_URL_มาวางตรงนี้") throw new Error("ยังไม่ได้ตั้งค่า GAS_URL");
+    if (!GAS_URL) throw new Error("ยังไม่ได้ตั้งค่า GAS_URL");
     const response = await fetch(`${GAS_URL}?action=${action}`);
     return await response.json();
   },
   
   post: async (action, payload) => {
-    if (GAS_URL === "นำ_URL_มาวางตรงนี้") throw new Error("ยังไม่ได้ตั้งค่า GAS_URL");
+    if (!GAS_URL) throw new Error("ยังไม่ได้ตั้งค่า GAS_URL");
     const response = await fetch(GAS_URL, {
       method: "POST",
       body: JSON.stringify({ action, payload }),
